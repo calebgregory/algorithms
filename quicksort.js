01adaa7a5182fe35a9cd84e0c88069b8db7e7a6e
calebgregory <calebgregory@gmail.com>
2015-09-08 20:40:51 -0500

    Making this thing's output less verbose

--- quicksort.js
+++ quicksort.js
@@ -5,18 +5,14 @@ var myA = [3,7,8,5,2,1,9,5,4];
 // here, e.g.,
 //    myA = _.shuffle(_.range(100));
 
-console.log('\n0>',myA)
+console.log('\n0>',myA,'\n')
 
 var count = 0;
-var iter = 0;
-var rec = 0;
 
 
 var quicksort = a => {
 
   var hi = a.length-1;
-
-  console.log('\n $.            %d                 no. times quicksort called\n',++iter);
 
   if(a.length < 2) {
     console.log('>> > > > > > > > > > > > > > > >>',a);
@@ -25,8 +21,6 @@ var quicksort = a => {
 
   var lo = 0;
   while (hi >= lo) {
-
-    console.log(' ~*                               inside while loop ... index of: hi : [ %d ] | lo : [ %d ]',hi,lo);
 
     while(a[hi] < a[lo]) {
 
@@ -56,8 +50,6 @@ var quicksort = a => {
   var lower = a.slice(0,hi);
   var upper = a.slice(hi+1);
 
-  console.log('\n...$                              calling quicksort: '+(++rec))
-
   var b = quicksort(lower)
     .concat(a[hi])
     .concat(quicksort(upper));
@@ -79,12 +71,12 @@ function print(a,hi,i,threeway) {
   if(threeway) {
     // when you see it printed it makes more sense
     return console.log(
-    "\n > ,- "+a[i]+" <- "+a[hi-1]+" <- "+a[hi]+" <-,             "+a[hi]+"<"+a[i]+" ? "+(a[hi]<a[i])+"\n"+
+    " > ,- "+a[i]+" <- "+a[hi-1]+" <- "+a[hi]+" <-,             "+a[hi]+"<"+a[i]+" ? "+(a[hi]<a[i])+"\n"+
     " > '-. lo, hi-1, hi .-\n");
   } else if(hi) {
     // same
     return console.log(
-    "\n > ,- "+a[i]+" <- "+a[hi]+" <-,                   "+a[hi]+"<"+a[i]+" ? "+(a[hi]<a[i])+"\n"+
+    " > ,- "+a[i]+" <- "+a[hi]+" <-,                  "+a[hi]+"<"+a[i]+" ? "+(a[hi]<a[i])+"\n"+
                       " > '-. lo, hi .-\n");
   } else {
     return console.log(++count+'>', a, '\n');
